c6f09b54e4c7321833814c0f7016032d94956fbf
David Rose <doppler@gmail.com>
2019-02-15 16:55:21 -0500

    Change # of tiles based on window size

--- src/Background.js
+++ src/Background.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Background = () => (
-  <div id="Background">
-    {Array.from({ length: 100 }).map((_, i) => (
-      <div className="Cell" key={i} />
-    ))}
-  </div>
-);
+const Background = () => {
+  const [numSquares, setNumSquares] = useState(10);
+
+  const changeNumSquares = () => {
+    const b = document.getElementById("Background");
+    const squareWidth = b.offsetWidth / 10 - 2;
+    setNumSquares((b.offsetHeight / squareWidth) * 10 + 10);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      changeNumSquares();
+    });
+    changeNumSquares();
+  }, []);
+
+  return (
+    <div id="Background">
+      {Array.from({ length: numSquares }).map((_, i) => (
+        <div className="Cell" key={i} />
+      ))}
+    </div>
+  );
+};
 
 export default Background;
