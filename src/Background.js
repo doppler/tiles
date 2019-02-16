31f0debbaab32d41dddb8db7b6f31363e29d9af9
David Rose <doppler@gmail.com>
2019-02-16 07:22:30 -0500

    animate rotation of tiles

--- src/Background.js
+++ src/Background.js
@@ -2,12 +2,21 @@ import React, { useState, useEffect } from "react";
 
 const Background = () => {
   const [numSquares, setNumSquares] = useState(10);
+  const [rotation, setRotation] = useState(0);
 
   const changeNumSquares = () => {
     const b = document.getElementById("Background");
     const squareWidth = b.offsetWidth / 10 - 2;
     setNumSquares((b.offsetHeight / squareWidth) * 10 + 10);
   };
+
+  const incrementRotation = () => {
+    setRotation(rotation === 359 ? 0 : rotation + 1);
+  };
+
+  useEffect(() => {
+    requestAnimationFrame(incrementRotation);
+  }, [rotation]);
 
   useEffect(() => {
     window.addEventListener("resize", () => {
@@ -23,7 +32,7 @@ const Background = () => {
           className="Cell"
           key={i}
           style={{
-            transform: `rotate(${(numSquares / 90) * 9 * i}deg)`
+            transform: `rotate(${i + rotation}deg)`
           }}
         />
       ))}
